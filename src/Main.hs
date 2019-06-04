f34ca6a3967180e6eb020b4af08b6f6a733ace44
sozoro <46480375+sozoro@users.noreply.github.com>
2019-06-05 01:20:05 +0900

    modifyL

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,7 @@ module Main where
 import qualified Data.Matrix as M
 import Control.Monad
 import Control.Applicative
-import Data.Maybe (listToMaybe)
+import Data.Maybe (listToMaybe,catMaybes)
 
 data Clockwise = L | U | R | D deriving (Eq, Enum, Bounded)
 instance Show Clockwise where
@@ -30,9 +30,16 @@ cyclicToEnum i = toEnum $ i `mod` length ([minBound .. maxBound :: a])
 
 modifyL :: ([a] -> [a]) -> (Int, Int) -> M.Matrix a -> M.Matrix a
 modifyL f (x, y) m = M.matrix (M.nrows m) (M.ncols m) $ \(r,c) ->
-  maybe (M.unsafeGet r c m) id $ guard (c == y) >> listToMaybe (drop r ls)
+  maybe (M.unsafeGet r c m) id $ guard (r == x) >> ls `safeIx` (fixedY - c)
   where
-    ls = f $ map (\x' -> M.getElem x' y m) [x .. M.nrows m]
+    fixedY = if y > M.nrows m then M.nrows m else y
+    ls     = f $ map (\y' -> M.unsafeGet x y' m) $ enumToFrom fixedY 1
+
+enumToFrom :: Enum a => a -> a -> [a]
+enumToFrom a = enumFromThenTo a $ pred a
+
+safeIx :: [a] -> Int -> Maybe a
+safeIx ls i = guard (i >= 0) >> listToMaybe (drop i ls)
 
 _4x4 :: M.Matrix Clockwise
 _4x4 = M.fromList 4 4 $ iterate cyclicSucc L
