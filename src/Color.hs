3d3d29b537b5ed84e24c7de7e483315045f9a45b
sozoro <46480375+sozoro@users.noreply.github.com>
2019-06-12 09:29:06 +0000

    fix

--- src/Color.hs
+++ src/Color.hs
@@ -13,6 +13,7 @@ import Data.Foldable (asum,elem)
 import Data.Maybe (listToMaybe,maybeToList,catMaybes)
 import qualified Control.Exception         as E
 import qualified Data.Colour.SRGB          as C
+import qualified Data.Colour.RGBSpace      as C
 import qualified System.Console.ANSI       as A
 import qualified System.Console.ANSI.Types as A
 
@@ -29,9 +30,6 @@ instance Semigroup a => Semigroup (V2 a) where
 instance Monoid a => Monoid (V2 a) where
   mempty = V2 mempty mempty
 
-fb :: V2 A.ConsoleLayer
-fb = V2 A.Foreground A.Background
-
 type Color     = C.Colour Float
 type ColorDiff = Maybe Color
 
@@ -45,12 +43,6 @@ update :: ChangeColor -> ColorDiff -> ColorDiff
 update Reset        = const Nothing
 update Through      = id
 update (NewColor c) = const $ Just c
-
-hasReset :: Foldable f => f ChangeColor -> Bool
-hasReset = foldr f False
-  where
-    f Reset = const True
-    f _     = id
 
 type FBColorDiff   = V2 ColorDiff
 type FBChangeColor = V2 ChangeColor
@@ -67,6 +59,9 @@ data ColorChar = ColorChar
   } deriving (Show, Eq)
 
 newtype ColorStr = ColorStr { colorChars :: [ColorChar] }
+
+class ColorShow a where
+  colorShow :: a -> ColorStr
 
 class Monad m => MonadAddCharStr m where
   addChar :: Char   -> m ()
@@ -91,10 +86,9 @@ toSRGB6Level :: (RealFrac b, Floating b)
 toSRGB6Level = C.toSRGBBounded
 
 setColor6Level :: ColorSetter
-setColor6Level layer color =
-  A.SetPaletteColor layer $ A.xterm6LevelRGB r g b
-  where
-    C.RGB r g b = fromIntegral <$> toSRGB6Level color
+setColor6Level layer color
+  = A.SetPaletteColor layer $ C.uncurryRGB A.xterm6LevelRGB
+  $ fromIntegral <$> toSRGB6Level color
 
 diff :: (Applicative f, Eq a) => f a -> f a -> f (Maybe a)
 diff x = (diff' <$> x <*>)
@@ -107,16 +101,16 @@ colorChar (ColorChar col cha) = do
   old <- get
   let new = update <$> col <*> old
       dif = old `diff` new
-  when (dif /= fmap (const Nothing) old) $ do
-    when (Just Nothing `elem` dif)
-      $ lift $ lift $ addStr $ A.setSGRCode [A.Reset]
-    let changings = asum $ maybeToList
-                        <$> ((\a -> fmap (a,)) <$> fb <*> fmap join dif)
-    setter <- lift ask
-    when (not $ null changings)
-      $ lift $ lift $ addStr $ A.setSGRCode $ uncurry setter <$> changings
-    put new
+  when (Just Nothing `elem` dif)
+    $ lift $ lift $ addStr $ A.setSGRCode [A.Reset]
+  setter <- lift ask
+  let fb        = V2 A.Foreground A.Background
+      changings = asum $ maybeToList
+                      <$> ((\a -> fmap (a,)) <$> fb <*> fmap join dif)
+  when (not $ null changings)
+    $ lift $ lift $ addStr $ A.setSGRCode $ uncurry setter <$> changings
   lift $ lift $ addChar cha
+  put new
 
 colorStr :: MonadAddCharStr m => ColorStr -> WithColor m ()
 colorStr = flip evalStateT (V2 Nothing Nothing)
@@ -143,9 +137,6 @@ withColor setter m = E.onException (runReaderT m setter)
 
 monochroStrs :: [(FBChangeColor, String)] -> ColorStr
 monochroStrs = ColorStr . join . fmap (uncurry $ fmap . ColorChar)
-
-class ColorShow a where
-  colorShow :: a -> ColorStr
 
 _24bitFullColorStream :: ColorStr
 _24bitFullColorStream = ColorStr
