fffccfb4b00a5003bee1b51e3e949599dd54cf7d
sozoro <46480375+sozoro@users.noreply.github.com>
2019-07-28 23:17:13 +0900

    default

--- default.nix
+++ default.nix
@@ -1,9 +1,7 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864"}:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865"}:
 let
   haskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: {
-      ansi-terminal     = self.callPackage ./nix/ansi-terminal-0.9.1.nix {};
-      concurrent-output = self.callPackage ./nix/concurrent-output-1.10.10.nix {};
     };
   };
   developPackage = haskellPackages.developPackage { root = ./.; };
